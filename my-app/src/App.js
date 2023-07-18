76ff45f477549e9d7804bf408df84b764ecb4383
Riya Patel <i.riya@atyantik.com>
2023-07-18 13:02:33 +0530

    Added Routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+// import Counter from './Components/Counter';
+// import CounterApp from "./Components/CounterApp";
 // import TodoForm from './Components/TodoApp/TodoForm';
 import TodoList from './Components/TodoApp/TodoList';
-
+// import Todo from './Components/TodoApp/Todo';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
+    // <div className="App">
+    //   {/* <CounterApp /> */}
+    //   {/* <Counter /> */}
+    //   <TodoList />
+    // </div>
     <Routes>
       <Route path='/' element={<TodoList />}></Route>
       {/* <Route path='todo-list' element={<TodoList />}></Route> */}
